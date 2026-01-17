ae79f009012a3b1aa66ddfdfb4198405230c49b1
sohelsamy1git remote add origin https://github.com/sohelsamy1/portfo.gitgit remote add origin https://github.com/sohelsamy1/portfo.git <sumon33021@gmail.com>
2026-01-17 13:11:53 +0600

    feat: implement user logout using sanctum token

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Cache;
 use App\Http\Requests\Auth\LoginRequest;
 use App\Http\Resources\User\UserResource;
 use App\Http\Requests\Auth\RegisterRequest;
@@ -11,6 +12,7 @@ use App\Http\Controllers\API\Base\BaseApiController;
 
 class AuthController extends BaseApiController
 {
+      //Register
     public function register(RegisterRequest $request)
     {
        $user = User::create([
@@ -21,7 +23,7 @@ class AuthController extends BaseApiController
        ]);
        return $this->success(new UserResource($user), 'User registered successfully',201);
     }
-
+       //Login
      public function login(LoginRequest $request)
     {
         $user = User::where('email', $request->email)->first();
@@ -37,10 +39,23 @@ class AuthController extends BaseApiController
             'token' => $token,
         ], 'Login successful');
     }
-
+       //Logout
+     public function logout(Request $request)
+    {
+        $user = $request->user();
+        $cacheKey = "user_profile_{$user->id}";
+        Cache::forget($cacheKey);
+        $request->user()->currentAccessToken()->delete();
+        return $this->success(null, 'Logout successful');
+    }
+        //Me
     public function me(Request $request)
     {
-
+        $user = $request->user();
+        $cacheKey = "user_profile_{$user->id}";
+        $cached = Cache::remember($cacheKey,now()->addHour(1),function () use ($user){
+        return new UserResource($user);
+        });
         return $this->success(new UserResource($request->user()), 'User profile');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,7 @@ Route::post('/register',[AuthController::class, 'register']);
 Route::post('/login',[AuthController::class, 'login']);
 
   Route::middleware('auth:sanctum')->group(function (){
+  Route::post('/logout',[AuthController::class, 'logout']);
     Route::get('/me',[AuthController::class, 'me']);
   });
 });
